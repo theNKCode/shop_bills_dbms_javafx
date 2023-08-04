edd22dd493da27070e3f0f73ad0a251e09b6d803
Nikhil Kamble <nikhilkamblestudy@gmail.com>
2023-08-04 07:57:09 +0530

    Logout funtionality added

--- src/main/java/com/example/shop_bills_javafx/dashboardController.java
+++ src/main/java/com/example/shop_bills_javafx/dashboardController.java
@@ -1,17 +1,24 @@
 package com.example.shop_bills_javafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class dashboardController implements Initializable {
@@ -971,52 +978,52 @@ public class dashboardController implements Initializable {
 //            orderDisplayTotal();
 //        }
 //    }
-//
-//    private double x = 0;
-//    private double y = 0;
-//
-//    public void logout() {
-//        try {
-//            Alert alert = new Alert(AlertType.CONFIRMATION);
-//            alert.setTitle("Confirmation Message");
-//            alert.setHeaderText(null);
-//            alert.setContentText("Are you sure you want to logout?");
-//            Optional<ButtonType> option = alert.showAndWait();
-//
-//            if (option.get().equals(ButtonType.OK)) {
-//                logout.getScene().getWindow().hide();
-//
-//                // LINK YOUR LOGIN FORM
-//                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-//                Stage stage = new Stage();
-//                Scene scene = new Scene(root);
-//
-//                root.setOnMousePressed((MouseEvent event) -> {
-//                    x = event.getSceneX();
-//                    y = event.getSceneY();
-//                });
-//
-//                root.setOnMouseDragged((MouseEvent event) -> {
-//                    stage.setX(event.getScreenX() - x);
-//                    stage.setY(event.getScreenY() - y);
-//
-//                    stage.setOpacity(.8f);
-//                });
-//
-//                root.setOnMouseReleased((MouseEvent event) -> {
-//                    stage.setOpacity(1);
-//                });
-//
-//                stage.initStyle(StageStyle.TRANSPARENT);
-//
-//                stage.setScene(scene);
-//                stage.show();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
+
+    private double x = 0;
+    private double y = 0;
+
+    public void logout() {
+        try {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to logout?");
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if (option.get().equals(ButtonType.OK)) {
+                logout.getScene().getWindow().hide();
+
+                // LINK YOUR LOGIN FORM
+                Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+                Stage stage = new Stage();
+                Scene scene = new Scene(root);
+
+                root.setOnMousePressed((MouseEvent event) -> {
+                    x = event.getSceneX();
+                    y = event.getSceneY();
+                });
+
+                root.setOnMouseDragged((MouseEvent event) -> {
+                    stage.setX(event.getScreenX() - x);
+                    stage.setY(event.getScreenY() - y);
+
+                    stage.setOpacity(.8f);
+                });
+
+                root.setOnMouseReleased((MouseEvent event) -> {
+                    stage.setOpacity(1);
+                });
+
+                stage.initStyle(StageStyle.TRANSPARENT);
+
+                stage.setScene(scene);
+                stage.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void displayUsername() {
         String user = data.username;
         user = user.substring(0, 1).toUpperCase() + user.substring(1);
